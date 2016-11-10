7aa19753b44ac8f977a46f9813d96b914b0a88d4
zyfncg <1370305206@qq.com>
2016-11-10 17:21:49 +0800

    3任务调度

--- Evalboards/Microsoft/Windows/OS2/app.c
+++ Evalboards/Microsoft/Windows/OS2/app.c
@@ -55,10 +55,16 @@
 static  CPU_STK  AppTaskStartStk[APP_TASK_START_STK_SIZE];
 OS_STK TaskStk11[APP_TASK_START_STK_SIZE];
 OS_STK TaskStk12[APP_TASK_START_STK_SIZE];
-
+OS_STK TaskStk21[APP_TASK_START_STK_SIZE];
+OS_STK TaskStk22[APP_TASK_START_STK_SIZE];
+OS_STK TaskStk23[APP_TASK_START_STK_SIZE];
 
 INT32U task11Time[3];
 INT32U task12Time[3];
+INT32U task21Time[3];
+INT32U task22Time[3];
+INT32U task23Time[3];
+INT32U StartTime = 10;
 
 /*
 *********************************************************************************************************
@@ -71,7 +77,12 @@ INT32U task12Time[3];
 static  void  AppTaskStart(void  *p_arg);
 void Task11(void *p_arg);
 void Task12(void *p_arg);
+void Task21(void *p_arg);
+void Task22(void *p_arg);
+void Task23(void *p_arg);
 void createTaskSet1();
+void createTaskSet2();
+
 /*
 *********************************************************************************************************
 *                                                main()
@@ -102,7 +113,8 @@ int  main (void)
 	//	(void          *)0,
 	//	(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
 	
-	createTaskSet1();
+	//createTaskSet1();
+	createTaskSet2();
 
     OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II).  */
 }
@@ -129,7 +141,6 @@ static void createTaskSet1(){
 
 	INT32U C11 = 1, C12 = 3;
 	INT32U T11 = 3, T12 = 6;
-	INT32U StartTime = 10;
 
 	task11Time[0] = C11;
 	task11Time[1] = StartTime;
@@ -156,6 +167,53 @@ static void createTaskSet1(){
 		(OS_STK        *)&TaskStk12[0],
 		(INT32U)APP_TASK_START_STK_SIZE,
 		(void          *)task12Time,
+		(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+}
+
+static void createTaskSet2() {
+
+
+	INT32U C21 = 1, C22 = 3, C23 = 4;
+	INT32U T21 = 3, T22 = 6, T23 = 9;
+
+	task21Time[0] = C21;
+	task21Time[1] = StartTime;
+	task21Time[2] = StartTime + T21;
+
+	task22Time[0] = C22;
+	task22Time[1] = StartTime;
+	task22Time[2] = StartTime + T22;
+
+	task23Time[0] = C23;
+	task23Time[1] = StartTime;
+	task23Time[2] = StartTime + T23;
+
+	OSTaskCreateExt((void(*)(void *))Task21,              /* Create the start task                                */
+		(void          *)0,
+		(OS_STK        *)&TaskStk21[APP_TASK_START_STK_SIZE - 1],
+		(INT8U)1u,
+		(INT16U)1u,
+		(OS_STK        *)&TaskStk21[0],
+		(INT32U)APP_TASK_START_STK_SIZE,
+		(void          *)task21Time,
+		(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+	OSTaskCreateExt((void(*)(void *))Task22,              /* Create the start task                                */
+		(void          *)0,
+		(OS_STK        *)&TaskStk22[APP_TASK_START_STK_SIZE - 1],
+		(INT8U)2u,
+		(INT16U)2u,
+		(OS_STK        *)&TaskStk22[0],
+		(INT32U)APP_TASK_START_STK_SIZE,
+		(void          *)task22Time,
+		(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+	OSTaskCreateExt((void(*)(void *))Task23,              /* Create the start task                                */
+		(void          *)0,
+		(OS_STK        *)&TaskStk23[APP_TASK_START_STK_SIZE - 1],
+		(INT8U)3u,
+		(INT16U)3u,
+		(OS_STK        *)&TaskStk23[0],
+		(INT32U)APP_TASK_START_STK_SIZE,
+		(void          *)task23Time,
 		(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
 }
 
@@ -188,8 +246,8 @@ void Task11(void *p_arg){
 
 	int toDelay;
 	int T = task11Time[2] - task11Time[1];
-	if (OSTimeGet()<10){
-		OSTimeDly(10 - OSTimeGet());
+	if (OSTimeGet()<StartTime){
+		OSTimeDly(StartTime - OSTimeGet());
 	}
 
 	while (1){
@@ -212,8 +270,8 @@ void Task12(void *p_arg) {
 	int toDelay;
 	int T = task12Time[2] - task12Time[1];
 
-	if (OSTimeGet()<10){
-		OSTimeDly(10 - OSTimeGet());
+	if (OSTimeGet()<StartTime){
+		OSTimeDly(StartTime - OSTimeGet());
 	}
 	while (1) {
 		while (OSTCBCur->CompTime>0) {
@@ -223,10 +281,98 @@ void Task12(void *p_arg) {
 		task12Time[1] = task12Time[2];
 		task12Time[2] = task12Time[1] + T;
 		OSTCBCur->CompTime = task12Time[0];
-		OSTCBCur->StartTime = task11Time[1];
+		OSTCBCur->StartTime = task12Time[1];
 		OSTCBCur->Deadline = task12Time[2];
 		//printf("task2     current:%d  deadline:%d\n", OSTimeGet(), OSTCBCur->Deadline);
 		OSTimeDly(toDelay);
+	}
+
+}
+void Task21(void *p_arg) {
+
+	int toDelay;
+	int c = task21Time[0];
+	int T = task21Time[2] - task21Time[1];
+
+	if (OSTimeGet()<StartTime) {
+		OSTimeDly(StartTime - OSTimeGet());
+	}
+	while (1) {
+		while (OSTCBCur->CompTime>0) {
+
+		}
+		toDelay = OSTCBCur->Deadline - OSTimeGet();
+		if (toDelay < 0) {
+			printf("task1 ÑÓÊ±£¡\n");
+			OSTCBCur->CompTime = c;
+			OSTCBCur->StartTime = OSTimeGet();
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+		}
+		else {
+			OSTCBCur->CompTime = task21Time[0];
+			OSTCBCur->StartTime = OSTCBCur->Deadline;
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+			OSTimeDly(toDelay);
+		}	
+		
+	}
+
+}
+void Task22(void *p_arg) {
+
+	int toDelay;
+	int c = task22Time[0];
+	int T = task22Time[2] - task22Time[1];
+
+	if (OSTimeGet()<StartTime) {
+		OSTimeDly(StartTime - OSTimeGet());
+	}
+	while (1) {
+		while (OSTCBCur->CompTime>0) {
+
+		}
+		toDelay = OSTCBCur->Deadline - OSTimeGet();
+		if (toDelay < 0) {
+			printf("task1 ÑÓÊ±£¡\n");
+			OSTCBCur->CompTime = c;
+			OSTCBCur->StartTime = OSTimeGet();
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+		}
+		else {
+			OSTCBCur->CompTime = task22Time[0];
+			OSTCBCur->StartTime = OSTCBCur->Deadline;
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+			OSTimeDly(toDelay);
+		}
+	}
+
+}
+void Task23(void *p_arg) {
+
+	int toDelay;
+	int c = task23Time[0];
+	int T = task23Time[2] - task23Time[1];
+
+	if (OSTimeGet()<StartTime) {
+		OSTimeDly(StartTime - OSTimeGet());
+	}
+	while (1) {
+		while (OSTCBCur->CompTime>0) {
+
+		}
+		toDelay = OSTCBCur->Deadline - OSTimeGet();
+		if (toDelay < 0) {
+			printf("task1 ÑÓÊ±£¡\n");
+			OSTCBCur->CompTime = c;
+			OSTCBCur->StartTime = OSTimeGet();
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+		}
+		else {
+			OSTCBCur->CompTime = task23Time[0];
+			OSTCBCur->StartTime = OSTCBCur->Deadline;
+			OSTCBCur->Deadline = OSTCBCur->StartTime + T;
+			OSTimeDly(toDelay);
+		}
 	}
 
 }
